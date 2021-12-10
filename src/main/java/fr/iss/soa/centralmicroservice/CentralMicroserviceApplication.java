30f903a5a0d61276fd8787ae25a77998b16aed49
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2021-12-10 09:24:50 +0100

    chore: generate spring boot project

---
+++ src/main/java/fr/iss/soa/centralmicroservice/CentralMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package fr.iss.soa.centralmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/iss/soa/centralmicroservice/CentralMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iss.soa.centralmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
