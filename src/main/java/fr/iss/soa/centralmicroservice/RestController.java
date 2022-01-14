e2c60fd1ac843e6979c5bc048dc2f1a61f140959
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2022-01-14 13:31:02 +0100

    feat: add alarm post endpoint

--- src/main/java/fr/iss/soa/centralmicroservice/RestController.java
+++ src/main/java/fr/iss/soa/centralmicroservice/RestController.java
@@ -1,9 +1,13 @@
 package fr.iss.soa.centralmicroservice;
 
+import fr.iss.soa.centralmicroservice.constants.MicroserviceType;
 import fr.iss.soa.centralmicroservice.errors.RoomNotFoundException;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -35,6 +39,18 @@ public class RestController {
 			servicesController.addPresenceDetectionEvent(id);
 		} else {
 			throw new RoomNotFoundException(id);
+		}
+	}
+
+	@PostMapping(value="/alarms/{id}")
+	public void setAlarm(@PathVariable int id, @RequestBody String requestBody) {
+		JSONObject jsonBody = new JSONObject(requestBody);
+		try {
+			boolean enabled = jsonBody.getBoolean("enabled");
+			Optional<Room> room = servicesController.getRoomOfId(id);
+			room.ifPresent(value -> servicesController.toggleMicroservice(value, MicroserviceType.ALARM, enabled));
+		} catch(JSONException e) {
+			e.printStackTrace();
 		}
 	}
 
--- src/main/java/fr/iss/soa/centralmicroservice/ServicesController.java
+++ src/main/java/fr/iss/soa/centralmicroservice/ServicesController.java
@@ -6,6 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.*;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -33,7 +34,7 @@ public class ServicesController {
 		}, 0, 5 * 1000);
 	}
 
-	private void toggleMicroservice(Room room, MicroserviceType type, boolean enabled) {
+	public void toggleMicroservice(Room room, MicroserviceType type, boolean enabled) {
 		RestTemplate restTemplate = new RestTemplate();
 		String url = MicroservicesUrl.getServiceIdUrl(type, room.getId());
 		String requestJson = "{\"enabled\": " + enabled + "}";
@@ -52,39 +53,42 @@ public class ServicesController {
 	private void fetchMicroservice(MicroserviceType type) {
 		RestTemplate restTemplate = new RestTemplate();
 		String url = MicroservicesUrl.getServiceUrl(type);
-
-		String results = restTemplate.getForObject(url, String.class);
-		System.out.println(results);
 		try {
-			// Build the room list from JSON response
-			JSONArray jsonResponse = new JSONArray(results);
-			jsonResponse.forEach(data -> {
-				JSONObject json = (JSONObject) data;
-				long id = json.getLong("id");
-				boolean enabled;
-				if (type == MicroserviceType.DOOR) {
-					enabled = json.getBoolean("locked");
-				} else {
-					enabled = json.getBoolean("enabled");
-				}
-				Optional<Room> room = getRoomOfId(id);
-				if (room.isPresent()) {
-					room.get().setStatus(type, enabled);
-				} else {
-					switch (type) {
-						case LIGHT:
-							roomList.add(new Room(id, enabled, false, false));
-							break;
-						case ALARM:
-							roomList.add(new Room(id, false, enabled, false));
-							break;
-						case DOOR:
-							roomList.add(new Room(id, false, false, enabled));
-							break;
+			String results = restTemplate.getForObject(url, String.class);
+			System.out.println(results);
+			try {
+				// Build the room list from JSON response
+				JSONArray jsonResponse = new JSONArray(results);
+				jsonResponse.forEach(data -> {
+					JSONObject json = (JSONObject) data;
+					long id = json.getLong("id");
+					boolean enabled;
+					if (type == MicroserviceType.DOOR) {
+						enabled = json.getBoolean("locked");
+					} else {
+						enabled = json.getBoolean("enabled");
 					}
-				}
-			});
-		} catch (JSONException e) {
+					Optional<Room> room = getRoomOfId(id);
+					if (room.isPresent()) {
+						room.get().setStatus(type, enabled);
+					} else {
+						switch (type) {
+							case LIGHT:
+								roomList.add(new Room(id, enabled, false, false));
+								break;
+							case ALARM:
+								roomList.add(new Room(id, false, enabled, false));
+								break;
+							case DOOR:
+								roomList.add(new Room(id, false, false, enabled));
+								break;
+						}
+					}
+				});
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		} catch (ResourceAccessException e) {
 			e.printStackTrace();
 		}
 	}
